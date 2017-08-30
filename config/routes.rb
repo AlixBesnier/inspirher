24983ceeac52aa468af5b601b67d9722cf36d76c
Alix Besnier <alix.besnier@gmail.com>
2017-08-30 22:50:20 +0200

    ready for interviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
   root to: 'pages#home'
+  get 'project', to: 'pages#project'
+  get 'team', to: 'pages#team'
+  get 'why', to: 'pages#why'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
